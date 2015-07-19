dc216131664c6b79a1517ff38bd898b1def57457
Valerio <thor486@gmail.com>
2015-07-19 16:50:07 +0200

    initial project

---
+++ SlidingDownController/SlidingDownController.h
@@ -0,0 +1,19 @@
+//
+//  SlidingDownController.h
+//  SlidingDownController
+//
+//  Created by Valerio Donfrancesco on 19/07/15.
+//  Copyright (c) 2015 Valerio Donfrancesco. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SlidingDownController.
+FOUNDATION_EXPORT double SlidingDownControllerVersionNumber;
+
+//! Project version string for SlidingDownController.
+FOUNDATION_EXPORT const unsigned char SlidingDownControllerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SlidingDownController/PublicHeader.h>
+
+
